9543e6250e4143d862bf41957064aa0691911284
Tushar Gupta <tushars2609@gmail.com>
2024-09-07 01:15:53 +0530

    Set serverside session cookies for authentication

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, request, abort, jsonify
+from flask import Flask, request, abort, jsonify, session
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS
+from flask_session import Session
 from config import ApplicationConfig
 from models import db, User
 
@@ -7,10 +9,27 @@ app = Flask(__name__)
 app.config.from_object(ApplicationConfig)
 
 bcrypt = Bcrypt(app)
+CORS(app, supports_credentials=True)
+server_session = Session(app)
 db.init_app(app)
 
 with app.app_context():
     db.create_all()
+
+@app.route("/@me", methods=["GET"])
+def get_current_user():
+    user_id = session.get("user_id")
+
+    if not user_id:
+        return jsonify({
+            "error": "Unauthorized"
+        }), 401
+    
+    user = User.query.filter_by(id=user_id).first()
+    return jsonify({
+        "id": user.id,
+        "email": user.email
+    })
 
 @app.route("/register", methods=["POST"])
 def register_user():
@@ -43,6 +62,8 @@ def login_user():
     if not bcrypt.check_password_hash(user.password, password):
         return jsonify({"error": "Unauthorized"}), 401
     
+    session["user_id"] = user.id
+
     return jsonify({
         "id": user.id,
         "email": user.email
